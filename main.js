d9714484a573b1466d317a0cfb4e941f3747d940
AreenHalabe <93481480+AreenHalabe@users.noreply.github.com>
2026-07-26 18:14:14 +0300

    update inestgrame name

--- main.js
+++ main.js
@@ -216,7 +216,7 @@ app.whenReady().then(async () => {
 
 
 
-    mainWindow.webContents.openDevTools(); // for dev tools
+   // mainWindow.webContents.openDevTools(); // for dev tools
 
     ipcMain.handle("print-invoice", async (event, html) => {
       try {
